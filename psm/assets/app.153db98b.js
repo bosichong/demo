2093e1baca65709dccecc279817e22b3429344a8
J.sky <bosichong@qq.com>
2023-05-22 19:18:03 +0800

    更新程序

--- psm/assets/app.153db98b.js
+++
@@ -1,1 +0,0 @@
-import{G as a}from"./index.f8592ff2.js";const s=a("app",{state:()=>({printPreviewPapers:[]}),actions:{navigateToPrint(t,e,p){this.$patch(r=>{r.printPreviewPapers=p,t.push({path:"/print",query:{fileName:e}})})}}});export{s as u};
